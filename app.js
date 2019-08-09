3c6b6ee4bb2af92d105ed2028c01cb8a0abb48e0
Ninad Hatankar <hatankarninad26@gmail.com>
2019-08-09 11:40:01 +0530

    mongoose connect error handling

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ cloudinary.config({
 });
 
 //DATABASEURL variable for mongoAtlas
-mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/mongoDemo_v7");
+mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/mongoDemo_v7", { useNewUrlParser: true }).
+  catch(error => handleError(error));
 
 
 //passport config
